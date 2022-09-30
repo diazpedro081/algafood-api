af31f066e5c4253b8ee087151226e07fec1c5708
diazpedrodev <081diazpedro@gmail.com>
2022-09-30 16:14:33 -0300

    alterando metodos jpa
    busca, adiciona, altera e salva.

--- src/main/java/br/com/algaworks/algafood/jpa/AlteracaoCozinhaMain.java
+++ src/main/java/br/com/algaworks/algafood/jpa/AlteracaoCozinhaMain.java
@@ -7,7 +7,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import br.com.algaworks.algafood.AlgafoodApiApplication;
 import br.com.algaworks.algafood.domain.model.Cozinha;
 
-public class InclusaoConsultaCozinhaMain {
+public class AlteracaoCozinhaMain {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
@@ -16,18 +16,12 @@ public class InclusaoConsultaCozinhaMain {
 
         CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 
-        Cozinha cozinha1 = new Cozinha();
-        cozinha1.setNome("Brasileira");
+        Cozinha cozinha = new Cozinha();
+        cozinha.setId(1L);
+        cozinha.setNome("Brasileira");
 
-        Cozinha cozinha2 = new Cozinha();
-        cozinha2.setNome("Japonesa");
+        cadastroCozinha.salvar(cozinha);
 
-        cozinha1 = cadastroCozinha.salvar(cozinha1);
-        cozinha2 = cadastroCozinha.salvar(cozinha2);
-
-        System.out.printf("%d - %s \n",cozinha1.getId(), cozinha1.getNome());
-
-        System.out.printf("%d - %s ",cozinha2.getId(), cozinha2.getNome());
     }
 
 }
--- src/main/java/br/com/algaworks/algafood/jpa/BuscaCozinhaMain.java
+++ src/main/java/br/com/algaworks/algafood/jpa/BuscaCozinhaMain.java
@@ -7,27 +7,18 @@ import org.springframework.context.ConfigurableApplicationContext;
 import br.com.algaworks.algafood.AlgafoodApiApplication;
 import br.com.algaworks.algafood.domain.model.Cozinha;
 
-public class InclusaoConsultaCozinhaMain {
+public class BuscaCozinhaMain {
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
         ConfigurableApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
                 .web(WebApplicationType.NONE)
                 .run(args);
 
         CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 
-        Cozinha cozinha1 = new Cozinha();
-        cozinha1.setNome("Brasileira");
+        Cozinha cozinha = cadastroCozinha.buscar(1L);
 
-        Cozinha cozinha2 = new Cozinha();
-        cozinha2.setNome("Japonesa");
-
-        cozinha1 = cadastroCozinha.salvar(cozinha1);
-        cozinha2 = cadastroCozinha.salvar(cozinha2);
-
-        System.out.printf("%d - %s \n",cozinha1.getId(), cozinha1.getNome());
-
-        System.out.printf("%d - %s ",cozinha2.getId(), cozinha2.getNome());
+        System.out.println(cozinha.getNome());
     }
 
 }
