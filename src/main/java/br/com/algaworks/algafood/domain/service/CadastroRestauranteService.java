8131b435697a9ef616c303937b02a85ef6f07755
diazpedrodev <081diazpedro@gmail.com>
2022-10-10 14:17:45 -0300

    cozinha - SDJ

--- src/main/java/br/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
+++ src/main/java/br/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -20,12 +20,8 @@ public class CadastroRestauranteService {
 
     public Restaurante salvar(Restaurante restaurante) {
         Long cozinhaId = restaurante.getCozinha().getId();
-        Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
-
-        if (cozinha == null) {
-            throw new EntidadeNaoEncontradaException(
-                    String.format("Não existe cadastro de cozinha com código: %d", cozinhaId));
-        }
+        Cozinha cozinha = cozinhaRepository.findById(cozinhaId)
+            .orElseThrow(() -> new EntidadeNaoEncontradaException((String.format("Nào existe cadastro de cozinha com código: %d", cozinhaId))));
 
         restaurante.setCozinha(cozinha);
 
