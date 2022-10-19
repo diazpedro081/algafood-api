996119b6cd19d559b54b27eff449c84ced05dcba
diazpedrodev <081diazpedro@gmail.com>
2022-10-19 11:42:14 -0300

    Mapeamento de projeto
    Grupo, permissao, produto , restaurante e usuarion.

--- src/main/java/br/com/algaworks/algafood/domain/model/Grupo.java
+++ src/main/java/br/com/algaworks/algafood/domain/model/Grupo.java
@@ -1,10 +1,16 @@
 package br.com.algaworks.algafood.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,7 +18,7 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Permissao {
+public class Grupo {
 
     @Id
     @EqualsAndHashCode.Include
@@ -20,11 +26,10 @@ public class Permissao {
     private Long id;
 
     @Column(nullable = false)
-    private Grupo grupo;
-
-    @Column(nullable = false)
     private String nome;
-    
-    @Column(nullable = false)
-    private String descricao;
+
+    @ManyToMany
+    @JoinTable(name = "grupo_permissao", joinColumns = @JoinColumn(name = "grupo_id"), inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+    private List<Permissao> permissoes = new ArrayList<>();
+
 }
--- src/main/java/br/com/algaworks/algafood/domain/model/Produto.java
+++ src/main/java/br/com/algaworks/algafood/domain/model/Produto.java
@@ -1,10 +1,14 @@
 package br.com.algaworks.algafood.domain.model;
 
+import java.math.BigDecimal;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,19 +16,26 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Permissao {
+public class Produto {
 
-    @Id
     @EqualsAndHashCode.Include
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
-    private Grupo grupo;
+    private String nome;
 
     @Column(nullable = false)
-    private String nome;
-    
-    @Column(nullable = false)
     private String descricao;
+
+    @Column(nullable = false)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private Boolean ativo;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Restaurante restaurante;
 }
