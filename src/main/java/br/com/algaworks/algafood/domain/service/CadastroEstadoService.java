862aedf43183fa3e1d3400bdc735e3755a63381b
diazpedrodev <081diazpedro@gmail.com>
2022-10-25 15:06:09 -0300

    refatorando os serviços ESTADO

--- src/main/java/br/com/algaworks/algafood/domain/service/CadastroEstadoService.java
+++ src/main/java/br/com/algaworks/algafood/domain/service/CadastroEstadoService.java
@@ -13,22 +13,31 @@ import br.com.algaworks.algafood.domain.repository.EstadoRepository;
 @Service
 public class CadastroEstadoService {
 
+    private static final String MSG_ESTADO_EM_USO = "Estado de código: %d não pode ser removido, pois esta está em uso";
+    private static final String MSG_ESTADO_NAO_ENCONTRADO = "Não existe um cadastro de Estado com código: %d";
     @Autowired
     private EstadoRepository estadoRepository;
 
     public Estado salvar(Estado estado) {
-		return estadoRepository.save(estado);
-	}
+        return estadoRepository.save(estado);
+    }
 
     public void excluir(Long estadoId) {
         try {
             estadoRepository.deleteById(estadoId);
         } catch (EmptyResultDataAccessException e) {
             throw new EntidadeNaoEncontradaException(
-                    String.format("Não existe um cadastro de Estado com código: %d", estadoId));
+                    String.format(MSG_ESTADO_NAO_ENCONTRADO, estadoId));
         } catch (DataIntegrityViolationException e) {
             throw new EntidadeEmUsoException(
-                    String.format("Estado de código: %d não pode ser removido, pois esta está em uso", estadoId));
+                    String.format(MSG_ESTADO_EM_USO, estadoId));
         }
     }
+
+    public Estado buscarOuFalhar(Long estadoId) {
+        return estadoRepository.findById(estadoId)
+                .orElseThrow(
+                        () -> new EntidadeNaoEncontradaException(String.format(MSG_ESTADO_NAO_ENCONTRADO, estadoId)));
+    }
+
 }
