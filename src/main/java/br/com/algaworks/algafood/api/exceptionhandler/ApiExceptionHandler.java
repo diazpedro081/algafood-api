56704df02b30d505f9176d22affe3590ffcd5602
diazpedrodev <081diazpedro@gmail.com>
2022-10-27 11:38:20 -0300

    Criando um exception handler global com ResponseEntityExceptionHandler

--- src/main/java/br/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -4,61 +4,51 @@ import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import br.com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import br.com.algaworks.algafood.domain.exception.NegocioException;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(EntidadeNaoEncontradaException.class)
-    public ResponseEntity<?> tratarEntidadeNaoEncontradaException(
-            EntidadeNaoEncontradaException e) {
+        @ExceptionHandler(EntidadeNaoEncontradaException.class)
+        public ResponseEntity<?> tratarEntidadeNaoEncontradaException(
+                        EntidadeNaoEncontradaException e) {
 
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage()).build();
+                Problema problema = Problema.builder()
+                                .dataHora(LocalDateTime.now())
+                                .mensagem(e.getMessage()).build();
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(problema);
-    }
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                                .body(problema);
+        }
 
-    @ExceptionHandler(EntidadeEmUsoException.class)
-    public ResponseEntity<?> tratarEntidadeEmUsoException(
-            EntidadeEmUsoException e) {
+        @ExceptionHandler(EntidadeEmUsoException.class)
+        public ResponseEntity<?> tratarEntidadeEmUsoException(
+                        EntidadeEmUsoException e) {
 
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage()).build();
+                Problema problema = Problema.builder()
+                                .dataHora(LocalDateTime.now())
+                                .mensagem(e.getMessage()).build();
 
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(problema);
-    }
+                return ResponseEntity.status(HttpStatus.CONFLICT)
+                                .body(problema);
+        }
 
-    @ExceptionHandler(NegocioException.class)
-    public ResponseEntity<?> tratarNegocioException(
-            NegocioException e) {
+        @ExceptionHandler(NegocioException.class)
+        public ResponseEntity<?> tratarNegocioException(
+                        NegocioException e) {
 
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage()).build();
+                Problema problema = Problema.builder()
+                                .dataHora(LocalDateTime.now())
+                                .mensagem(e.getMessage()).build();
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(problema);
-    }
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                                .body(problema);
+        }
 
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException() {
-
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem("O tipo de mídia não é aceito").build();
-
-        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-                .body(problema);
-    }
 }
