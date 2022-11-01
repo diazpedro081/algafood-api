759a30b9336493b958a70061c600e0b26384b5c3
diazpedrodev <081diazpedro@gmail.com>
2022-11-01 10:27:08 -0300

    Refatoração Cozinha Entity

--- src/main/java/br/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/br/com/algaworks/algafood/domain/model/Cozinha.java
@@ -20,16 +20,16 @@ import lombok.EqualsAndHashCode;
 @Entity
 public class Cozinha {
 
-    @EqualsAndHashCode.Include
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
-    @Column(nullable = false)
-    private String nome;
-
-    @JsonIgnore 
-    @OneToMany(mappedBy = "cozinha")
-    private List<Restaurante> restaurantes = new ArrayList<>();
-
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
+	
 }
