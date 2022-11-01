570326f980cb0d6fe1433cbdc2a283cdd0cfb451
diazpedrodev <081diazpedro@gmail.com>
2022-11-01 10:36:07 -0300

    Refatoração ItemPedido Entity

--- src/main/java/br/com/algaworks/algafood/domain/model/ItemPedido.java
+++ src/main/java/br/com/algaworks/algafood/domain/model/ItemPedido.java
@@ -5,9 +5,9 @@ import java.math.BigDecimal;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Id;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -15,12 +15,17 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class ItemPedido {
+public class ItemPedido { 
 
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long id;
+
+    private BigDecimal precoUnitario;
+    private BigDecimal precoTotal;
+    private Integer quantidade;
+    private String observacao;
 
     @ManyToOne
     @JoinColumn(nullable = false)
@@ -29,10 +34,5 @@ public class ItemPedido {
     @ManyToOne
     @JoinColumn(nullable = false)
     private Produto produto;
-
-    private BigDecimal precoUnitario;
-    private BigDecimal precoTotal;
-    private Integer quantidade;
-    private String observacao;
 
 }
