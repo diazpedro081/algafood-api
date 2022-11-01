c5a3c7b4b43311a24069b1f7238095e97a68319a
diazpedrodev <081diazpedro@gmail.com>
2022-11-01 10:36:15 -0300

    Refatoração Pedido Entity

--- src/main/java/br/com/algaworks/algafood/domain/model/Pedido.java
+++ src/main/java/br/com/algaworks/algafood/domain/model/Pedido.java
@@ -5,15 +5,16 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -23,40 +24,40 @@ import lombok.EqualsAndHashCode;
 @Entity
 public class Pedido {
 
-    @EqualsAndHashCode.Include
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
-    
-    private BigDecimal subTotal;
-    private BigDecimal taxaFrete;
-    private BigDecimal valorTotal;
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private BigDecimal subtotal;
+	private BigDecimal taxaFrete;
+	private BigDecimal valorTotal;
 
-    @Embedded
-    private Endereco endereçoEntrega;
+	@Embedded
+	private Endereco enderecoEntrega;
+	
+	private StatusPedido status;
+	
+	@CreationTimestamp
+	private LocalDateTime dataCriacao;
 
-    private StatusPedido status;
-
-    @Column(nullable = false)
-    private LocalDateTime dataCriacao;
-
-    private LocalDateTime dataConfirmacao;
-    private LocalDateTime dataCancelamento;
-    private LocalDateTime dataEntrega;
-
-    @OneToMany(mappedBy = "pedido")
-    private List<ItemPedido> itens = new ArrayList<>();
-
-    @ManyToOne
-    @JoinColumn(nullable = false)
-    private FormaPagamento formaPagamento;
-
-    @ManyToOne
-    @JoinColumn(nullable = false)
-    private Restaurante restaurante;
-
-    @ManyToOne
-    @JoinColumn(name = "usuario_id_cliente", nullable = false)
-    private Usuario cliente;
+	private LocalDateTime dataConfirmacao;
+	private LocalDateTime dataCancelamento;
+	private LocalDateTime dataEntrega;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private FormaPagamento formaPagamento;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Restaurante restaurante;
+	
+	@ManyToOne
+	@JoinColumn(name = "usuario_cliente_id", nullable = false)
+	private Usuario cliente;
+	
+	@OneToMany(mappedBy = "pedido")
+	private List<ItemPedido> itens = new ArrayList<>();
 
 }
