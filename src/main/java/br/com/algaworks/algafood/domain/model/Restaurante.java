f46959993e925390485738df86b132e39a6ad0fe
diazpedrodev <081diazpedro@gmail.com>
2022-11-09 14:32:41 -0300

    Criando constraints de validação customizadas usando composição

--- src/main/java/br/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/br/com/algaworks/algafood/domain/model/Restaurante.java
@@ -19,7 +19,6 @@ import javax.persistence.OneToMany;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PositiveOrZero;
 import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
 
@@ -29,6 +28,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import br.com.algaworks.algafood.core.validation.Groups;
+import br.com.algaworks.algafood.core.validation.TaxaFrete;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -46,7 +46,7 @@ public class Restaurante {
 	@Column(nullable = false)
 	private String nome;
 	
-	@PositiveOrZero
+	@TaxaFrete
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 
