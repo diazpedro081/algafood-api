10772b1c45e119733e16aab1abc5d76bd96731ea
diazpedrodev <081diazpedro@gmail.com>
2022-11-12 09:38:58 -0300

    Executando processo de validação programaticamente

---
+++ src/main/java/br/com/algaworks/algafood/core/validation/ValidacaoException.java
@@ -0,0 +1,14 @@
+package br.com.algaworks.algafood.core.validation;
+
+import org.springframework.validation.BindingResult;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ValidacaoException extends RuntimeException {
+      
+    private BindingResult bindingResult;  
+     
+}
