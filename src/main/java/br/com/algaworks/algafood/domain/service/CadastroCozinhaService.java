1f8147dbe22ef939b9fc596179e0018b789df2bd
diazpedrodev <081diazpedro@gmail.com>
2022-11-20 12:09:23 -0300

    service cozinha cadastro

--- src/main/java/br/com/algaworks/algafood/domain/service/CadastroCozinhaService.java
+++ src/main/java/br/com/algaworks/algafood/domain/service/CadastroCozinhaService.java
@@ -13,31 +13,32 @@ import br.com.algaworks.algafood.domain.repository.CozinhaRepository;
 @Service
 public class CadastroCozinhaService {
 
-	private static final String MSG_COZINHA_EM_USO = "Cozinha de código %d não pode ser removida, pois está em uso";
+	private static final String MSG_COZINHA_EM_USO 
+		= "Cozinha de código %d não pode ser removida, pois está em uso";
 
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
-
+	
 	public Cozinha salvar(Cozinha cozinha) {
 		return cozinhaRepository.save(cozinha);
 	}
-
+	
 	public void excluir(Long cozinhaId) {
 		try {
 			cozinhaRepository.deleteById(cozinhaId);
-
+			
 		} catch (EmptyResultDataAccessException e) {
 			throw new CozinhaNaoEncontradaException(cozinhaId);
-
+		
 		} catch (DataIntegrityViolationException e) {
 			throw new EntidadeEmUsoException(
-					String.format(MSG_COZINHA_EM_USO, cozinhaId));
+				String.format(MSG_COZINHA_EM_USO, cozinhaId));
 		}
 	}
-
+	
 	public Cozinha buscarOuFalhar(Long cozinhaId) {
 		return cozinhaRepository.findById(cozinhaId)
-				.orElseThrow(() -> new CozinhaNaoEncontradaException(cozinhaId));
+			.orElseThrow(() -> new CozinhaNaoEncontradaException(cozinhaId));
 	}
-
+	
 }
