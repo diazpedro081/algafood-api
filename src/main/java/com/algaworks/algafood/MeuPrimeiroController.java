bb70396431a645ce7903a62669cc340ea64f2af2
diazpedrodev <081diazpedro@gmail.com>
2023-02-25 06:11:09 -0300

    Update MeuPrimeiroController.java

--- src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -4,11 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+//Informa que essa é uma classe responsavel por receber requisições web
 @Controller
 public class MeuPrimeiroController {
     
     //Modelo padrão para requisitar itens
-    @GetMapping("/hello")
+    @GetMapping("/hello") //caminho
     @ResponseBody //Indica que a String deve ser a resposta para requisição
     public String hello(){
         return "Hello!";
