ded7405899c3a20a4ba553a6278eb1b6f14e6de2
diazpedrodev <081diazpedro@gmail.com>
2023-02-24 22:38:04 -0300

    Update MeuPrimeiroController.java

--- src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -1,8 +1,10 @@
 package com.algaworks.algafood;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class MeuPrimeiroController {
     
     //Modelo padrão para requisitar itens
