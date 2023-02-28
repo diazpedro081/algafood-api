0c2525c2f0005b7b6b9704e2918a779b61c04505
diazpedrodev <081diazpedro@gmail.com>
2023-02-28 04:24:40 -0300

    Create NotificadorEmail.java

---
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -0,0 +1,14 @@
+package com.algaworks.algafood.di.notificacao;
+
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.di.modelo.Cliente;
+
+@Component // Aqui informamos que essa classe é um componente do Spring
+public class NotificadorEmail {
+
+    public void notificar(Cliente cliente, String mensagem) {
+        System.out.printf("Notificando %s atraves do email %s: %s\n",
+                cliente.getNome(), cliente.getEmail(), mensagem);
+    }
+}
