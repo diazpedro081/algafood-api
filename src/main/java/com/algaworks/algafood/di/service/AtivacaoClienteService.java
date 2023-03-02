fff21963b535396c2603cda58a8163c5a23bf723
diazpedrodev <081diazpedro@gmail.com>
2023-03-02 04:33:20 -0300

    Update AtivacaoClienteService.java

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,11 +1,10 @@
 package com.algaworks.algafood.di.service;
 
-import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.NotificadorEmail;
 
-@Component
+
 public class AtivacaoClienteService {
 
     private NotificadorEmail notificador;
