33f0a5b94fa14ad27bcb3d9611f3d7c2643d8312
diazpedrodev <081diazpedro@gmail.com>
2023-03-04 05:09:07 -0300

    Update AtivacaoClienteService.java

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,16 +1,24 @@
 package com.algaworks.algafood.di.service;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.NotificadorEmail;
 
-
+@Component
 public class AtivacaoClienteService {
 
     private NotificadorEmail notificador;
 
+    @Autowired // define o construtor primario
     public AtivacaoClienteService(NotificadorEmail notificador) {
         this.notificador = notificador;
+    }
+
+    public AtivacaoClienteService(String qualquer) {
+        System.out.println("Qualquer coisa '-'");
     }
 
     public void ativar(Cliente cliente) {
