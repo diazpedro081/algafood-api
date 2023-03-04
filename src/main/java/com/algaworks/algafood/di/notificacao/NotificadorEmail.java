9904312ec9f7229dde0a9bad253149b14fd56af1
diazpedrodev <081diazpedro@gmail.com>
2023-03-04 04:49:28 -0300

    Update NotificadorEmail.java

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -4,27 +4,12 @@ import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 
-// @Component  Aqui informamos que essa classe é um componente do Spring
+@Component // Aqui informamos que essa classe é um componente do Spring
 public class NotificadorEmail implements Notificador {
-
-    private boolean caixaAlta;
-    private String hostServidorSmtp;
-
-    public NotificadorEmail(String hostServidorSmtp) {
-        this.hostServidorSmtp = hostServidorSmtp; 
-        System.out.println("NotificadorEmail");
-    }
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        if (this.caixaAlta) {
-            mensagem = mensagem.toUpperCase();
-        }
-        System.out.printf("Notificando %s atraves do email %s usando SMTP %s: %s\n",
-                cliente.getNome(), cliente.getEmail(),this.hostServidorSmtp ,mensagem);
-    }
-
-    public void setCaixaAlta(boolean caixaAlta) {
-        this.caixaAlta = caixaAlta;
+        System.out.printf("Notificando %s atraves do email %s: %s\n",
+                cliente.getNome(), cliente.getEmail(), mensagem);
     }
 }
